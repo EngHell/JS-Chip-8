668d194920adfcd11f5790a973a7f76ec5577679
Angel Recinos <migueln6@gmail.com>
2017-08-29 17:33:43 -0600

    added test for 0x8XY0

--- tests/js/test_opcodes_8.js
+++ tests/js/test_opcodes_8.js
@@ -5,4 +5,15 @@ describe("opcodes 0x8XXX", function () {
         c8 = chip8();
         c8.initialize();
     });
+
+    // 0x8XY0: assing vx to the value of vy
+    it("0x8XY0: X = 2, Y = 15, VY = 20, so VX = 20", function () {
+       c8.memory[512] = 0x82;
+       c8.memory[513] = 0xf0;
+       c8.V[15] = 20;
+
+       c8.emulateCycle();
+
+       expect(c8.V[2]).toEqual(20);
+    });
 });
