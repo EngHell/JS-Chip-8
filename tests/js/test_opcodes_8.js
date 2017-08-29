d76fff9ba1f5aeae60c8ef7ddc2d235505cfae33
Angel Recinos <migueln6@gmail.com>
2017-08-29 17:41:27 -0600

    added test for 0x8XY1

--- tests/js/test_opcodes_8.js
+++ tests/js/test_opcodes_8.js
@@ -16,4 +16,18 @@ describe("opcodes 0x8XXX", function () {
 
        expect(c8.V[2]).toEqual(20);
     });
+
+    // 0x8XY1: sets VX to (VX or VY)
+    it("0x8XY1: X = 1, Y = 0, VX = 00010001, VY = 00000010, so VX = 00010011", function () {
+        c8.memory[512] = 0x81;
+        c8.memory[513] = 0x01;
+        c8.V[1] = parseInt("00010001", 2);
+        c8.V[0] = parseInt("00000010", 2);
+
+        c8.emulateCycle();
+
+        expect(c8.V[1]).toEqual(parseInt("00010011",2));
+    })
+
+
 });
