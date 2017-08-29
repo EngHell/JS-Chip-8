e5add97277c3521fa859825ad23183f921290910
Angel Recinos <migueln6@gmail.com>
2017-08-29 17:48:50 -0600

    added test for 0x8XY2

--- tests/js/test_opcodes_8.js
+++ tests/js/test_opcodes_8.js
@@ -27,7 +27,17 @@ describe("opcodes 0x8XXX", function () {
         c8.emulateCycle();
 
         expect(c8.V[1]).toEqual(parseInt("00010011",2));
-    })
+    });
 
+    // 0x8XY2: VX = (VX and VY)
+    it("0x8XY2: X = 1, Y = 0, VX = 10010001, VY = 10000010, so VX = 10010011", function () {
+        c8.memory[512] = 0x81;
+        c8.memory[513] = 0x02;
+        c8.V[1] = parseInt("10010001", 2);
+        c8.V[0] = parseInt("10000010", 2);
 
+        c8.emulateCycle();
+
+        expect(c8.V[1]).toEqual(parseInt("10000000",2));
+    });
 });
